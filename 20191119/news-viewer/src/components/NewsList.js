9ad7d882276af00ecda42e442a1b7422b512564e
goodzzong <goodzzong@gmail.com>
2019-11-20 12:22:52 +0900

    complete news-viewer test

---
+++ 20191119/news-viewer/src/components/NewsList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
+const NewsListBlock = styled.div`
+	box-sizing: border-box;
+	padding-bottom: 3rem;
+	width: 768px;
+	margin: 0 auto;
+	margin-top: 2rem;
+
+	@media screen and (max-width: 768px) {
+		width: 100%;
+		padding-left: 1rem;
+		padding-right: 1rem;
+	}
+`;
+
+const NewsList = ({ category }) => {
+	const [loading, response, error] = usePromise(() => {
+		const query = category === 'all' ? '' : `&category=${category}`
+		return axios.get(
+			`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8b0f0ff05ff6496b8df6933732cbafde`
+		);
+	}, [category]);
+
+
+	if (loading) {
+		return <div>...대기중</div>;
+	}
+
+	if (!response) {
+		return null;
+	}
+
+	if (error) {
+		return <NewsListBlock>에러!</NewsListBlock>;
+	}
+
+	const { articles } = response.data;
+	return (
+		<NewsListBlock>
+			{articles.map(article => (
+				<NewsItem key={article.url} {...article} />
+			))}
+		</NewsListBlock>
+	);
+};
+
+export default NewsList;
